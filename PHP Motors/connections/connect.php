0e7d28ba6ba14753c1cdd4669bba76459fa6c843
Eric Shewchuk <eric@thebanyanteam.com>
2023-09-21 16:12:08 -1000

    created DB connection

---
+++ PHP Motors/connections/connect.php
@@ -0,0 +1,17 @@
+<?php function phpmotorsConnect(){
+ $server = 'localhost';
+ $dbname= 'phpmotors';
+ $username = 'iClient';
+ $password = 'Clunker08'; 
+ $dsn = "mysql:host=$server;dbname=$dbname";
+ $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
+
+ // Create the actual connection object and assign it to a variable
+ try {
+  $link = new PDO($dsn, $username, $password, $options);
+  return $link;
+ } catch(PDOException $e) {
+  header('Location: /phpmotors/view/500.php');
+  exit;
+ }
+} ?>
