6c1d123ee3910d6e3e66bd7651eab9a5ca43d9cc
Eric Shewchuk <eric@thebanyanteam.com>
2023-11-28 14:13:46 -1000

    deleted 'final_project' folder and contents

--- final_project/views/footer.php
+++
@@ -1,6 +0,0 @@
-</main>
-    <footer>
-    <p>&copy; <?php echo date("Y"); ?> | The Clever Primate</p>
-    </footer>
-</body>
-</html>
--- final_project/views/navigation.php
+++
@@ -1,8 +0,0 @@
-    <nav>
-        <a href="http://localhost:3000/final_project/home.php">Home</a>
-        <a href="#">Classic</a>
-        <a href="#">Sports</a>
-        <a href="#">SUV</a>
-        <a href="#">Trucks</a>
-        <a href="#">Used</a>
-    </nav>
