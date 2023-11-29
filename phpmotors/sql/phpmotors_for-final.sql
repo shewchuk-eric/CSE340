98fc88d468efee0d1fd5aac1ae8ef96c2786febb
Eric Shewchuk <eric@thebanyanteam.com>
2023-11-28 17:13:30 -1000

    backed up database

--- phpmotors/sql/phpmotors_for-final.sql
+++ phpmotors/sql/phpmotors_for-final.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3307
--- Generation Time: Nov 19, 2023 at 04:39 AM
+-- Generation Time: Nov 29, 2023 at 04:12 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -78,11 +78,90 @@ INSERT INTO `clients` (`clientId`, `clientFirstname`, `clientLastname`, `clientE
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `images`
+--
+
+CREATE TABLE `images` (
+  `imgId` int(10) UNSIGNED NOT NULL,
+  `invId` int(10) UNSIGNED NOT NULL,
+  `imgName` varchar(100) NOT NULL,
+  `imgPath` varchar(150) NOT NULL,
+  `imgDate` timestamp NOT NULL DEFAULT current_timestamp(),
+  `imgPrimary` tinyint(4) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `images`
+--
+
+INSERT INTO `images` (`imgId`, `invId`, `imgName`, `imgPath`, `imgDate`, `imgPrimary`) VALUES
+(7, 1, 'wrangler.jpg', '/phpmotors/images/vehicles/wrangler.jpg', '2023-11-24 01:00:03', 1),
+(8, 1, 'wrangler-tn.jpg', '/phpmotors/images/vehicles/wrangler-tn.jpg', '2023-11-24 01:00:03', 1),
+(9, 2, 'ford-modelt.jpg', '/phpmotors/images/vehicles/ford-modelt.jpg', '2023-11-24 01:01:28', 1),
+(10, 2, 'ford-modelt-tn.jpg', '/phpmotors/images/vehicles/ford-modelt-tn.jpg', '2023-11-24 01:01:28', 1),
+(11, 3, 'lambo-Adve.jpg', '/phpmotors/images/vehicles/lambo-Adve.jpg', '2023-11-24 01:01:41', 1),
+(12, 3, 'lambo-Adve-tn.jpg', '/phpmotors/images/vehicles/lambo-Adve-tn.jpg', '2023-11-24 01:01:41', 1),
+(13, 4, 'monster.jpg', '/phpmotors/images/vehicles/monster.jpg', '2023-11-24 01:02:39', 1),
+(14, 4, 'monster-tn.jpg', '/phpmotors/images/vehicles/monster-tn.jpg', '2023-11-24 01:02:39', 1),
+(15, 5, 'ms.jpg', '/phpmotors/images/vehicles/ms.jpg', '2023-11-24 01:03:51', 1),
+(16, 5, 'ms-tn.jpg', '/phpmotors/images/vehicles/ms-tn.jpg', '2023-11-24 01:03:51', 1),
+(17, 6, 'bat.jpg', '/phpmotors/images/vehicles/bat.jpg', '2023-11-24 01:04:01', 1),
+(18, 6, 'bat-tn.jpg', '/phpmotors/images/vehicles/bat-tn.jpg', '2023-11-24 01:04:01', 1),
+(19, 7, 'mm.jpg', '/phpmotors/images/vehicles/mm.jpg', '2023-11-24 01:04:15', 1),
+(20, 7, 'mm-tn.jpg', '/phpmotors/images/vehicles/mm-tn.jpg', '2023-11-24 01:04:15', 1),
+(21, 8, 'fire-truck.jpg', '/phpmotors/images/vehicles/fire-truck.jpg', '2023-11-24 01:04:34', 1),
+(22, 8, 'fire-truck-tn.jpg', '/phpmotors/images/vehicles/fire-truck-tn.jpg', '2023-11-24 01:04:34', 1),
+(23, 10, 'camaro.jpg', '/phpmotors/images/vehicles/camaro.jpg', '2023-11-24 01:05:58', 1),
+(24, 10, 'camaro-tn.jpg', '/phpmotors/images/vehicles/camaro-tn.jpg', '2023-11-24 01:05:58', 1),
+(25, 11, 'escalade.jpg', '/phpmotors/images/vehicles/escalade.jpg', '2023-11-24 01:06:25', 1),
+(26, 11, 'escalade-tn.jpg', '/phpmotors/images/vehicles/escalade-tn.jpg', '2023-11-24 01:06:25', 1),
+(27, 12, 'hummer.jpg', '/phpmotors/images/vehicles/hummer.jpg', '2023-11-24 01:06:36', 1),
+(28, 12, 'hummer-tn.jpg', '/phpmotors/images/vehicles/hummer-tn.jpg', '2023-11-24 01:06:36', 1),
+(29, 13, 'aerocar.jpg', '/phpmotors/images/vehicles/aerocar.jpg', '2023-11-24 01:06:47', 1),
+(30, 13, 'aerocar-tn.jpg', '/phpmotors/images/vehicles/aerocar-tn.jpg', '2023-11-24 01:06:47', 1),
+(31, 14, 'fbi.jpg', '/phpmotors/images/vehicles/fbi.jpg', '2023-11-24 01:07:01', 1),
+(32, 14, 'fbi-tn.jpg', '/phpmotors/images/vehicles/fbi-tn.jpg', '2023-11-24 01:07:01', 1),
+(33, 15, 'dogcar.jpg', '/phpmotors/images/vehicles/dogcar.jpg', '2023-11-24 01:07:12', 1),
+(34, 15, 'dogcar-tn.jpg', '/phpmotors/images/vehicles/dogcar-tn.jpg', '2023-11-24 01:07:12', 1),
+(35, 17, 'gremlin.jpg', '/phpmotors/images/vehicles/gremlin.jpg', '2023-11-24 01:07:25', 1),
+(36, 17, 'gremlin-tn.jpg', '/phpmotors/images/vehicles/gremlin-tn.jpg', '2023-11-24 01:07:25', 1),
+(37, 19, 'dart.jpg', '/phpmotors/images/vehicles/dart.jpg', '2023-11-24 01:07:37', 1),
+(38, 19, 'dart-tn.jpg', '/phpmotors/images/vehicles/dart-tn.jpg', '2023-11-24 01:07:37', 1),
+(39, 20, 'charger.jpg', '/phpmotors/images/vehicles/charger.jpg', '2023-11-24 01:07:50', 1),
+(40, 20, 'charger-tn.jpg', '/phpmotors/images/vehicles/charger-tn.jpg', '2023-11-24 01:07:50', 1),
+(41, 21, 'caravan.jpg', '/phpmotors/images/vehicles/caravan.jpg', '2023-11-24 01:08:03', 1),
+(42, 21, 'caravan-tn.jpg', '/phpmotors/images/vehicles/caravan-tn.jpg', '2023-11-24 01:08:03', 1),
+(43, 24, 'pinto.jpg', '/phpmotors/images/vehicles/pinto.jpg', '2023-11-24 01:08:14', 1),
+(44, 24, 'pinto-tn.jpg', '/phpmotors/images/vehicles/pinto-tn.jpg', '2023-11-24 01:08:14', 1),
+(47, 25, 'delorean-interior.jpg', '/phpmotors/images/vehicles/delorean-interior.jpg', '2023-11-24 01:21:29', 0),
+(48, 25, 'delorean-interior-tn.jpg', '/phpmotors/images/vehicles/delorean-interior-tn.jpg', '2023-11-24 01:21:29', 0),
+(49, 25, 'delorean-rear.jpg', '/phpmotors/images/vehicles/delorean-rear.jpg', '2023-11-24 01:21:41', 0),
+(50, 25, 'delorean-rear-tn.jpg', '/phpmotors/images/vehicles/delorean-rear-tn.jpg', '2023-11-24 01:21:41', 0),
+(51, 25, 'delorean.jpg', '/phpmotors/images/vehicles/delorean.jpg', '2023-11-24 01:22:19', 1),
+(52, 25, 'delorean-tn.jpg', '/phpmotors/images/vehicles/delorean-tn.jpg', '2023-11-24 01:22:19', 1),
+(53, 19, 'dart-rear.jpg', '/phpmotors/images/vehicles/dart-rear.jpg', '2023-11-24 01:26:43', 0),
+(54, 19, 'dart-rear-tn.jpg', '/phpmotors/images/vehicles/dart-rear-tn.jpg', '2023-11-24 01:26:43', 0),
+(55, 19, 'dart-interior.jpg', '/phpmotors/images/vehicles/dart-interior.jpg', '2023-11-24 01:26:53', 0),
+(56, 19, 'dart-interior-tn.jpg', '/phpmotors/images/vehicles/dart-interior-tn.jpg', '2023-11-24 01:26:53', 0),
+(57, 3, 'lambo-interior.jpg', '/phpmotors/images/vehicles/lambo-interior.jpg', '2023-11-24 01:29:08', 0),
+(58, 3, 'lambo-interior-tn.jpg', '/phpmotors/images/vehicles/lambo-interior-tn.jpg', '2023-11-24 01:29:08', 0),
+(59, 3, 'lambo-open.jpg', '/phpmotors/images/vehicles/lambo-open.jpg', '2023-11-24 01:29:18', 0),
+(60, 3, 'lambo-open-tn.jpg', '/phpmotors/images/vehicles/lambo-open-tn.jpg', '2023-11-24 01:29:18', 0),
+(61, 10, 'camaro-2.jpg', '/phpmotors/images/vehicles/camaro-2.jpg', '2023-11-24 01:31:27', 0),
+(62, 10, 'camaro-2-tn.jpg', '/phpmotors/images/vehicles/camaro-2-tn.jpg', '2023-11-24 01:31:27', 0),
+(63, 10, 'camaro-back.jpg', '/phpmotors/images/vehicles/camaro-back.jpg', '2023-11-24 01:31:39', 0),
+(64, 10, 'camaro-back-tn.jpg', '/phpmotors/images/vehicles/camaro-back-tn.jpg', '2023-11-24 01:31:39', 0),
+(65, 10, 'camaro-interior.png', '/phpmotors/images/vehicles/camaro-interior.png', '2023-11-24 01:31:48', 0),
+(66, 10, 'camaro-interior-tn.png', '/phpmotors/images/vehicles/camaro-interior-tn.png', '2023-11-24 01:31:48', 0);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `inventory`
 --
 
 CREATE TABLE `inventory` (
-  `invId` int(11) NOT NULL,
+  `invId` int(10) UNSIGNED NOT NULL,
   `invMake` varchar(30) NOT NULL,
   `invModel` varchar(30) NOT NULL,
   `invDescription` text NOT NULL,
@@ -117,7 +196,22 @@ INSERT INTO `inventory` (`invId`, `invMake`, `invModel`, `invDescription`, `invI
 (19, 'Dodge', 'Dart', 'Not the worst car you can find.', 'dart.jpg', 'dart-tn.jpg', 4500, 1, 'Dark Charcoal Gray', 5),
 (20, 'Dodge', 'Charger', 'You want muscle? Don&#039;t waste your time in the gym. Get this car today!', 'charger.jpg', 'charger-tn.jpg', 28000, 1, 'Army Green', 2),
 (21, 'Dodge', 'Caravan', 'Room for the whole crew.', 'caravan.jpg', 'caravan-tn.jpg', 9250, 3, 'White', 5),
-(24, 'Ford', 'Pinto', 'Ralph Nader called this car unsafe at any speed. He must be a Chevy guy.', 'pinto.jpg', 'pinto-tn.jpg', 1000, 1, 'White/Orange', 5);
+(24, 'Ford', 'Pinto', 'Ralph Nader called this car unsafe at any speed. He must be a Chevy guy.', 'pinto.jpg', 'pinto-tn.jpg', 1000, 1, 'White/Orange', 5),
+(25, 'DMC', 'DeLorean', 'Almost as fast as a lightning bolt.', 'delorean.jpg', 'delorean.jpg', 198100, 1, 'Stainless Steel', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reviews`
+--
+
+CREATE TABLE `reviews` (
+  `reviewId` int(10) UNSIGNED NOT NULL,
+  `reviewText` text CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `reviewDate` timestamp NOT NULL DEFAULT current_timestamp(),
+  `invId` int(10) UNSIGNED NOT NULL,
+  `clientId` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Indexes for dumped tables
@@ -137,11 +231,26 @@ ALTER TABLE `clients`
   ADD UNIQUE KEY `clientEmail` (`clientEmail`);
 
 --
+-- Indexes for table `images`
+--
+ALTER TABLE `images`
+  ADD PRIMARY KEY (`imgId`),
+  ADD KEY `FC_inv_images` (`invId`);
+
+--
 -- Indexes for table `inventory`
 --
 ALTER TABLE `inventory`
   ADD PRIMARY KEY (`invId`),
   ADD KEY `classificationId` (`classificationId`);
+
+--
+-- Indexes for table `reviews`
+--
+ALTER TABLE `reviews`
+  ADD PRIMARY KEY (`reviewId`),
+  ADD KEY `FK_reviews_inventory` (`invId`),
+  ADD KEY `FK_reviews_clients` (`clientId`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -160,20 +269,45 @@ ALTER TABLE `clients`
   MODIFY `clientId` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
+-- AUTO_INCREMENT for table `images`
+--
+ALTER TABLE `images`
+  MODIFY `imgId` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=67;
+
+--
 -- AUTO_INCREMENT for table `inventory`
 --
 ALTER TABLE `inventory`
-  MODIFY `invId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `invId` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+
+--
+-- AUTO_INCREMENT for table `reviews`
+--
+ALTER TABLE `reviews`
+  MODIFY `reviewId` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
 --
 
 --
+-- Constraints for table `images`
+--
+ALTER TABLE `images`
+  ADD CONSTRAINT `FC_inv_images` FOREIGN KEY (`invId`) REFERENCES `inventory` (`invId`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
 -- Constraints for table `inventory`
 --
 ALTER TABLE `inventory`
   ADD CONSTRAINT `inventory_ibfk_1` FOREIGN KEY (`classificationId`) REFERENCES `carclassification` (`classificationId`);
+
+--
+-- Constraints for table `reviews`
+--
+ALTER TABLE `reviews`
+  ADD CONSTRAINT `FK_reviews_clients` FOREIGN KEY (`clientId`) REFERENCES `clients` (`clientId`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_reviews_inventory` FOREIGN KEY (`invId`) REFERENCES `inventory` (`invId`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
